04910375355e572d459ccb8c4afe48baeec9c6ce
PIYUSH-SATIYA <pbsatiya007@gmail.com>
2025-07-18 00:32:54 +0530

    complete backend

---
+++ server/controllers/message.controller.js
@@ -0,0 +1,103 @@
+import cloudinary from "../lib/cloudinary.js";
+import User from "../models/User.model.js";
+import Message from "../models/message.model.js";
+import { io, userSocketMap } from "../server.js";
+
+
+// get all the users except the logged in user
+export const getUserForSidebar = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const filteredUser = await User.find({ _id: { $ne: userId } }).select(
+            "-password"
+        );
+
+        // count the number of messages not seen
+        const unseenMessages = filteredUser.map(async (user) => {
+            const messages = await Message.find({
+                senderId: user._id,
+                receiverId: userId,
+                seen: false,
+            });
+
+            if(messages.length > 0){
+                unseenMessages(user._id) = messages.length;
+            }
+        });
+
+        await Promise.all(promise)
+        res.json({success: true, users: filteredUsers, unseenMessages})
+    } catch (error) {
+        console.log(error.messages);
+        res.json({success: false, message: error.message})
+    }
+};
+
+
+// get all the messages for the selected user, i.e. open its chat
+
+export const getMessages = async (req, res) => {
+    try {
+        const {id: selectedUserId} = req.params;
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                {senderId: myId, receiverId: selectedUserId},
+                {senderId: selectedUserId, receiverId: myId},
+            ]
+        })
+
+        await Message.updateMany({senderId: selectedUserId, receiverId: myId}, {seen: true})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
+
+// api to mark messages as seen message id
+export const markMessageAsSeen = async (req, res) => {
+    try {
+        const {id} = req.params;
+        await Message.findByIdAndUpdate(id, {seen: true})
+        res.json({success: true})
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
+
+// send messages to selected user
+
+export const sendMessage = async (req, res) => {
+    try {
+        const {text, image} = req.body;
+        const receiverId = req.params.id
+        const senderId = req.user._id
+        
+
+        let imageUrl;
+        if(image){
+            const uploadResponse = await cloudinary.uploader.upload(image)
+            imageUrl = uploadResponse.secure_url;
+        }
+        const newMessage = await Message.create({
+            senderId,
+            receiverId,
+            text, 
+            image: imageUrl
+        })
+
+        // Emit new message to the recievers socket 
+        const recieversSocketId = userSocketMap[receiverId]
+        if (recieversSocketId) {
+            io.to(recieversSocketId).emit('newMessage', newMessage)
+        }
+
+        res.json({success: true, newMessage})
+
+
+    } catch (error) {
+        
+    }
+}
---
+++ server/lib/db.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+// Function to connect to mongoDB
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URI}`
+        );
+
+        console.log(
+            `DB connected | DB host: ${connectionInstance.connection.host}`
+        );
+
+        await mongoose.connect(`${process.env.MONGODB_URI}`);
+    } catch (error) {
+        console.log("Error connecting to DB: ", error.message);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
